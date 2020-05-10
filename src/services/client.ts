8dc581ec7c9aea4557f892c36fceb35ffcc6074f
Venancio Souza <venanciodesouza@icloud.com>
2020-05-10 10:35:32 -0300

    add dynamic uri

--- src/services/client.ts
+++ src/services/client.ts
@@ -1,10 +1,19 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
 import AsyncStorage from '@react-native-community/async-storage'
 import { WebSocketLink } from 'apollo-link-ws'
 import { split } from 'apollo-link'
 import { getMainDefinition } from 'apollo-utilities'
+
+var ip = null
+
+const customFetch = async (uri: any, options: any) => {
+  const rasp: any = await AsyncStorage.getItem('@action.raspberry')
+  const data = JSON.parse(rasp)
+  ip = data.ip
+  return fetch(`http://${data.ip}:4000/graphql`, options)
+}
 
 const cache = new InMemoryCache()
 
@@ -13,14 +22,14 @@ cache.writeData({
 })
 
 const wsLink = new WebSocketLink({
-  uri: `ws://192.168.2.161:4000/graphql`,
+  uri: `ws://${ip}:4000/graphql`,
   options: {
     reconnect: true,
   },
 })
 
-const httpLink = new HttpLink({
-  uri: 'http://192.168.2.161:4000/graphql',
+const httpLink = createHttpLink({
+  fetch: customFetch,
 })
 
 const link = split(
